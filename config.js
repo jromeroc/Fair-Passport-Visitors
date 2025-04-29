299eab9b8769f96a5990f07293528a68c8bdf6cb
Camilo Romero Cendales <jcamiloromeroc@gmail.com>
2025-04-28 21:47:30 -0500

    Update config
    Redireccionando a la API de producción

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // config.js
 export const CONFIG = {
-    BACKEND_URL:'http://localhost:5000/api'
-    //BACKEND_URL: 'https://api.tudominio.com/api'
+    //BACKEND_URL:'http://localhost:5000/api'
+    BACKEND_URL: 'https://fair-passport-api-690c36445400.herokuapp.com/'
   };
+  
   
